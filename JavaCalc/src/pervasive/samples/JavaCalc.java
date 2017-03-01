9690ab71dafe383e81c130395b24ab29650388c7
JacopoFabrini <jacfabr@live.it>
2017-03-01 11:29:53 +0100

    Hotfix for scanner and semplification of acquisition data for the user

--- JavaCalc/src/pervasive/samples/JavaCalc.java
+++ JavaCalc/src/pervasive/samples/JavaCalc.java
@@ -5,42 +5,33 @@ import java.util.Scanner;
 public class JavaCalc {
 
 	public static void main(String[] args) {
-		double n1, n2; // Double è un tipo primitivo e non deve essere creato,
-						// scanner invece è di tipo reference e deve essere
-						// creato
-		String operation; //
-		Scanner scannerObject = new Scanner(System.in); // Fa il parsing di un
-														// flusso in ingresso
+		// Questo è il modo corretto con cui utilizzare scanner
+		try (Scanner scanner = new Scanner(System.in)) {
+			
+			// Modo più intelligente di acquisizione informazioni
+			System.out.println("Enter an expression of the form 3 * 5");
+			
+			double n1 = scanner.nextDouble();
+			String operation = scanner.next();
+			double n2 = scanner.nextDouble();
 
-		System.out.println("Enter first number");
-		n1 = scannerObject.nextDouble(); // next double prende la stringa e la
-											// porta in double
-
-		System.out.println("Enter second number");
-		n2 = scannerObject.nextDouble();
-
-		Scanner op = new Scanner(System.in); // creiamo un altro oggetto di tipo
-												// scanner
-		System.out.println("Enter your operation");
-		operation = op.next(); // Qui non avviene alcuna conversione quindi
-								// usiamo solo next
-
-		switch (operation) {
-		case "+":
-			System.out.println("Your answer is " + (n1 + n2));
-			break;
-		case "-":
-			System.out.println("Your answer is " + (n1 - n2));
-			break;
-		case "*":
-			System.out.println("Your answer is " + (n1 * n2));
-			break;
-		case "/":
-			System.out.println("Your answer is " + (n1 / n2));
-			break;
-		default:
-			System.out.println("Wrong operator!");
-			break;
+			switch (operation) {
+			case "+":
+				System.out.println("Your answer is " + (n1 + n2));
+				break;
+			case "-":
+				System.out.println("Your answer is " + (n1 - n2));
+				break;
+			case "*":
+				System.out.println("Your answer is " + (n1 * n2));
+				break;
+			case "/":
+				System.out.println("Your answer is " + (n1 / n2));
+				break;
+			default:
+				System.out.println("Wrong operator!");
+				break;
+			}
 		}
 
 	}
